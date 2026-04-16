83402247f2b23cb761916b92fe93ba3d4e8a41c8
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-04-16 13:58:21 -0600

    fix: added optional parameter None to Nodo

--- BST/Nodo.py
+++ BST/Nodo.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 class Nodo[T]:
-    def __init__(self, valor:int, izquierda:Nodo[T], derecha:Nodo[T]) -> None:
+    def __init__(self, valor:int, izquierda:Nodo[T] | None, derecha:Nodo[T] | None) -> None:
         self.valor = valor
         self.izquierda = izquierda
         self.derecha = derecha
