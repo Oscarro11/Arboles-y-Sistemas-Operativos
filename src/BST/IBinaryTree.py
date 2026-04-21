2f7223791cca3de9acbde91b80bc9ae7221dffef
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-04-21 17:12:29 -0600

    fix(IBinaryTree): added proper typing for abstract methods

--- src/BST/IBinaryTree.py
+++ src/BST/IBinaryTree.py
@@ -1,29 +1,36 @@
 from typing import List, Tuple, TypeVar
+from abc import ABC, abstractmethod
 
 T = TypeVar("T")
 
-class IBinaryTree[T]:
+class IBinaryTree[T](ABC):
 
-    # Inserta un elemento. True si se insertó y false si ya existe.
+    # Inserta un elemento. True si se inserto y false si ya existe.
+    @abstractmethod
     def insertar (self, elemento: T) -> bool:
         pass
 
-    # Elimina un elemento. True si se eliminó y false si no existe.
+    # Elimina un elemento. True si se elimino y false si no existe.
+    @abstractmethod
     def eliminar (self, elemento: T) -> bool:
         pass
 
-    # Busca un elemento. Retorna si lo encontró y cuántas comparaciones hizo.
+    # Busca un elemento. Retorna si lo encontro y cuantas comparaciones hizo.
+    @abstractmethod
     def buscar (self, elemento: T) -> Tuple[bool, int]:
         pass
     
-    # Verifica si el elemento es la raíz.
+    # Verifica si el elemento es la raiz.
+    @abstractmethod
     def esRaiz (self, elemento: T) -> bool:
         pass
     
     # Verifica si el elemento es una hoja.
+    @abstractmethod
     def esHoja (self, elemento: T) -> bool:
         pass
     
-    # Retorna una lista en órden.
+    # Retorna una lista en orden.
+    @abstractmethod
     def inOrder(self) -> List[T]:
         pass
